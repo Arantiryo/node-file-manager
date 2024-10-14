aeb6c8547e57ee4cb89267cea2f9baf5f6b3aa78
Ivan Tsenilov <arantirex@gmail.com>
2024-10-15 01:40:56 +0400

    fix: get user name

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {
 } from "./src/utils.js";
 import { handleCommand } from "./src/command_handler.js";
 
-export const userName = process.argv?.[2].split("=")?.[1] || "user";
+export const userName = process.argv?.[2]?.split("=")?.[1] || "user";
 
 // Change working directory
 switchToHomeDir();
